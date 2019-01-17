74c2bebc5a6b371817f55c379dfecd6b9d5e9225
Nicholas Bowden <nsbowden@ucdavis.edu>
2019-01-16 16:58:05 -0800

    convert to function

--- get861.R
+++ get861.R
@@ -1,77 +1,85 @@
 ### Automating the download of EIA 861 zip files and the extraction of specific datasets and the append across years. 
 
-library(RCurl)
-library(readxl)
-library(XML)
+getEIA861 = function(datadir) {
 
-### Get the 861 Zip File Links from EIA
-url = "https://www.eia.gov/electricity/data/eia861/"
-page = getURLContent(url)
-html = htmlParse(page)
-tda = getNodeSet(html, "//table/tr/td[2]/a")
-urlTails = sapply(tda, function(x) xmlGetAttr(x, 'href'))
-links = paste(url, urlTails, sep="")
+  if(! "RCurl" %in% installed.packages()){
+  install.packages("RCurl")
+  } 
+  library(RCurl)
 
-### Need to loop over accounting for heterogeneity in the files.  PJM LoadDataRead addresses similar problem. 
-### First only years after 2001
+  if(! "XML" %in% installed.packages()){
+  install.packages("XML")
+  } 
+  library(XML)
 
-in1 = lapply(links, function(x) strsplit(x, 'f861')[[1]][2])
-in2 = lapply(in1, function(x) strsplit(x, '\\.')[[1]][1])
+  if(! "readxl" %in% installed.packages()){
+  install.packages("XML")
+  } 
+  library(readxl)
 
-in2 = lapply(in2, function(x) ifelse((nchar(x) == 2 & as.integer(x) < 90), paste0('20', x), x))
-in2 = lapply(in2, function(x) ifelse((nchar(x) == 2 & as.integer(x) > 20), paste0('19', x), x))
-
-### Look at in2 interactively to determine a cutoff
-### Here it is based on the genesis of retail power marketers
-
-outdir = "/home/nicholas/Documents/EIA861/eia861/download/"
-
-if(dir.exists(outdir)){
-  setwd(outdir)
-  } else {
-  dir.create(outdir)
-  setwd(outdir)
-}
-
-
-z = which(in2 == "2013")
-
-for (i in 1:z) {
-
-  temp = tempfile()
-  download.file(links[i], temp)
-  files = unzip(temp, list=TRUE)[[1]]
-  
-  if (i < which(in2 == "2012")) {
-  file = files[grepl("sales_ult_cust_2", files, ignore.case=TRUE)]
-  stateSheet = excel_sheets(unzip(temp, file, exdir = outdir, junkpaths=FALSE))[[1]]
-  d = data.frame(read_excel(unzip(temp, file, exdir = outdir, junkpaths=FALSE), sheet=stateSheet, skip = 2))
-  write.csv(d, paste0(outdir, "eia861Year", in2[i], ".csv"), row.names=FALSE)
-  unlink(temp)
-  file.remove(file)
-  }
-
-  if (i == which(in2 == "2012")) {
-  file = files[grepl("retail_sales_2012", files, ignore.case=TRUE)]
-  stateSheet = excel_sheets(unzip(temp, file, exdir = outdir, junkpaths=FALSE))[[1]]
-  d = data.frame(read_excel(unzip(temp, file, exdir = outdir, junkpaths=FALSE), sheet=stateSheet, skip = 2))
-  write.csv(d, paste0(outdir, "eia861Year", in2[i], ".csv"), row.names=FALSE)
-  unlink(temp)
-  file.remove(file)
-  }
-
-  if (i > which(in2 == "2012")) {
-  file = files[grepl("file2", files, ignore.case=TRUE)]
-  stateSheet = excel_sheets(unzip(temp, file, exdir = outdir, junkpaths=FALSE))[[1]]
-    if ( i < which(in2 == "2007")) {
-    d = data.frame(read_excel(unzip(temp, file, exdir = outdir, junkpaths=FALSE), sheet=stateSheet, skip = 2))
+  if(dir.exists(datadir)){
+    setwd(datadir)
     } else {
-    d = data.frame(read_excel(unzip(temp, file, exdir = outdir, junkpaths=FALSE), sheet=stateSheet))
-    }
-  write.csv(d, paste0(outdir, "eia861Year", in2[i], ".csv"), row.names=FALSE)
-  unlink(temp)
-  file.remove(file)
+    dir.create(datadir)
+    setwd(datadir)
   }
+
+  ### Get the 861 Zip File Links from EIA
+  url = "https://www.eia.gov/electricity/data/eia861/"
+  page = getURLContent(url)
+  html = htmlParse(page)
+  tda = getNodeSet(html, "//table/tr/td[2]/a")
+  urlTails = sapply(tda, function(x) xmlGetAttr(x, 'href'))
+  links = paste(url, urlTails, sep="")
+
+  ### Need to loop over accounting for heterogeneity in the file names (2012 versus 07), then make all 19XX ore 20XX.
+
+  in1 = lapply(links, function(x) strsplit(x, 'f861')[[1]][2])
+  in2 = lapply(in1, function(x) strsplit(x, '\\.')[[1]][1])
+
+  in2 = lapply(in2, function(x) ifelse((nchar(x) == 2 & as.integer(x) < 90), paste0('20', x), x))
+  in2 = lapply(in2, function(x) ifelse((nchar(x) == 2 & as.integer(x) > 20), paste0('19', x), x))
+
+  z = which(in2 == "2001")
+
+  for (i in 1:z) {
+
+    temp = tempfile()
+    download.file(links[i], temp)
+    files = unzip(temp, list=TRUE)[[1]]
+    
+    if (i < which(in2 == "2012")) {
+    file = files[grepl("sales_ult_cust_2", files, ignore.case=TRUE)]
+    stateSheet = excel_sheets(unzip(temp, file, exdir = datadir, junkpaths=FALSE))[[1]]
+    d = data.frame(read_excel(unzip(temp, file, exdir = datadir, junkpaths=FALSE), sheet=stateSheet, skip = 2))
+    write.csv(d, paste0(datadir, "eia861Year", in2[i], ".csv"), row.names=FALSE)
+    unlink(temp)
+    file.remove(file)
+    }
+
+    if (i == which(in2 == "2012")) {
+    file = files[grepl("retail_sales_2012", files, ignore.case=TRUE)]
+    stateSheet = excel_sheets(unzip(temp, file, exdir = datadir, junkpaths=FALSE))[[1]]
+    d = data.frame(read_excel(unzip(temp, file, exdir = datadir, junkpaths=FALSE), sheet=stateSheet, skip = 2))
+    write.csv(d, paste0(datadir, "eia861Year", in2[i], ".csv"), row.names=FALSE)
+    unlink(temp)
+    file.remove(file)
+    }
+
+    if (i > which(in2 == "2012")) {
+    file = files[grepl("file2", files, ignore.case=TRUE)]
+    stateSheet = excel_sheets(unzip(temp, file, exdir = datadir, junkpaths=FALSE))[[1]]
+      if ( i < which(in2 == "2007")) {
+      d = data.frame(read_excel(unzip(temp, file, exdir = datadir, junkpaths=FALSE), sheet=stateSheet, skip = 2))
+      } else {
+      d = data.frame(read_excel(unzip(temp, file, exdir = datadir, junkpaths=FALSE), sheet=stateSheet))
+      }
+    write.csv(d, paste0(datadir, "eia861Year", in2[i], ".csv"), row.names=FALSE)
+    unlink(temp)
+    file.remove(file)
+    }
+  }
+  unlink(list.dirs(), recursive=TRUE)
 }
 
-unlink(list.dirs(), recursive=TRUE)
+
